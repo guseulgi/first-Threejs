66bce7f2b1e51c4e59d6950580fa7cea1913a243
seulgi <guseul9405@gmail.com>
2023-06-10 14:03:23 +0900

    Three.js 설정 수정 및 vite 설치, 그리고 큐브 화면에 띄우기

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 import * as THREE from 'three';
+// import { GLTFLoader } from 'GLTFLoader';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+// const canvas = document.getElementById('canvas');
+// canvas.width = 600;
+// canvas.height = 600;
 
 // Scene
 let scene = new THREE.Scene();
@@ -13,3 +19,26 @@ let camera = new THREE.PerspectiveCamera(
 );
 
 camera.position.set(0, 0, 5);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+
+animate();
