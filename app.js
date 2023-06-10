2731bb1af73e0f98e50a99a02b8ea45d00f0b120
seulgi <guseul9405@gmail.com>
2023-06-10 13:41:13 +0900

    Three.js 설치 및 Scene 과 Camera 설정

---
+++ app.js
@@ -0,0 +1,15 @@
+import * as THREE from 'three';
+
+// Scene
+let scene = new THREE.Scene();
+scene.backgorund = new THREE.Color('#ccc');
+
+// Camera
+let camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+
+camera.position.set(0, 0, 5);
